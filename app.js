86d0ea51efaa972385da6538600ae6ecaedfc97b
AlexTaracido <199184168+AlexTaracido@users.noreply.github.com>
2026-03-12 19:25:51 -0500

    Version 22

--- app.js
+++ app.js
@@ -37,6 +37,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  const hiddenElements = document.querySelectorAll(".reveal");
+  hiddenElements.forEach((el) => observer.observe(el));
+
+});
